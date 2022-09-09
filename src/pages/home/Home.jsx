a13fb13b48fe40b20cf519dd4a898673d33fd9f2
exize <amio121212@gmail.com>
2022-09-09 21:16:13 +0300

    footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,10 @@ import Blog from "../../components/home/Blog"
 import ActionBarComponentProps from "../../components/caledarPayment/Calendar"
 import Aboutcard from "../../components/home/Aboutcard"
 import FolderList from "../../components/home/FolderList"
+import Collabs from "../../components/footer/Page9"
+import Footer from "../../components/footer/Footer"
 import "./home.css"
+
 import { Typography } from "@mui/material"
 import ArchiveIcon from '@mui/icons-material/Archive';
 import { Link } from "react-router-dom"
@@ -17,28 +20,34 @@ const Home = () => {
             <div id="head">
                 <p id="quote">"Coming together is a beginning. Keeping together is progress.</p>
                 <p id="punchline"> Working together is success." <span id="name">-Henry Ford</span></p>
-                
             </div>
-            <Typography><h1 id="h1">Welcom to our employee web!</h1></Typography> 
-            <div className="imgAndCard">
-                <img id="graph" alt="Infographic" data-id="1215165" data-animated-url="https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif" skip_resize="true" srcset="https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 320w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 400w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 450w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 640w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 700w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 800w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 768w" sizes="(max-width: 919px) 100vw, max(768px, 98vh)" src="https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif" width="732" height="558"/>            
-                <Aboutcard className="card"></Aboutcard>
+            <div className="img-card-container">
+                <Typography><h1 id="welcome">Welcom to our employee web!</h1></Typography> 
+                <div className="img-card">
+                    <div className="graph-container">
+                        <img  id="graph" alt="Infographic" data-id="1215165" data-animated-url="https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif" skip_resize="true" srcset="https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 320w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 400w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 450w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 640w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 700w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 800w, https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif 768w" sizes="(max-width: 919px) 100vw, max(768px, 98vh)" src="https://cdn.dribbble.com/users/14356/screenshots/1215165/media/1f109d7b20b3c2228a6e9f24b044d8e7.gif" width="732" height="558"/>            
+                    </div>
+                    <Aboutcard className="card"></Aboutcard>
+                </div>
             </div>
+            <Typography><h1 className="pulications-title">We want to keep you updated about our actions in the society</h1></Typography> 
             <MultiActionAreaCard/>
+            <Typography><h1 className="events-title">Company's events</h1></Typography> 
             <div className="calendarAndBlog">
                 <ActionBarComponentProps className="calendar"></ActionBarComponentProps>
-                <FolderList></FolderList>
-                <Blog></Blog>
+                <FolderList/>
+                <Blog/>
             </div>
             <div id="boxArchive">
                 <h1 id="explainArchive" >TO ARCHIVE</h1>
-                <Link to="/archive">
-                <ArchiveIcon id="archive">
-                </ArchiveIcon>
-                </Link>
-
+                <div className="archive-img">
+                    <Link to="/archive">
+                        <ArchiveIcon id="archive"/>
+                    </Link>
+                </div>
             </div>
-            
+            <Collabs/>
+            <Footer/>
         </div>
     )
 }
