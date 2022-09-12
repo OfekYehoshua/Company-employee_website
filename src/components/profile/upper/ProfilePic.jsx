8537a9b9092f0dd4f311b46e0bbd16a93acc6b0f
exize <amio121212@gmail.com>
2022-09-12 11:06:52 +0300

    orginize-files

--- src/components/profile/upper/ProfilePic.jsx
+++ src/components/profile/upper/ProfilePic.jsx
@@ -1,13 +1,13 @@
 import Avatar from "@mui/material/Avatar";
 
 const ProfilePic = (props) => {
-    return(
-        <div className="container">
-            <div className="profile-pic-container background-creme">
-                <Avatar className="profile-pic" alt="Remy Sharp" src={props.src}/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="profile-pic-container background-creme">
+        <Avatar className="profile-pic" alt="Remy Sharp" src={props.src} />
+      </div>
+    </div>
+  );
+};
 
-export default ProfilePic
+export default ProfilePic;
